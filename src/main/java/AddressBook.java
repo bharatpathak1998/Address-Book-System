630df9f6592eeab9d76cc52bbbb51193363c7447
bharatpathak1998 <bharatpathak099@gmail.com>
2023-01-03 21:56:00 +0530

    Added : Functionality To Add Multiple Contacts

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -2,15 +2,17 @@
  * @author Bharat Pathak
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
 
+    ArrayList<Address> list = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-    Address address = new Address();
 
-    // In this addDetails function we are set some data by user input :-
+    // Here we are create a function to add details in the arraylist by using user input :-
     public void addDetails() {
+        Address address = new Address();
 
         System.out.println("Enter First Name : ");
         address.setFirstName(sc.nextLine());
@@ -36,32 +38,107 @@ public class AddressBook {
         System.out.println("Enter Email Id : ");
         address.setEmail(sc.nextLine());
 
+        list.add(address);
         System.out.println(address);
+        System.out.println("Contact Added Successfully.");
     }
 
-    // In this editDetails function we are edit data by user input :-
+    // Here we are create a function to edit details from the arraylist by using user input :-
     public void editDetails() {
-        System.out.println("Enter Phone Number Of A Person To Edit :  ");
-        String editByPhoneNumber = sc.nextLine();
-        if (address.getPhoneNumber().equals(editByPhoneNumber)) {
-            addDetails();
+        boolean flag = false;
+        if (list.isEmpty()) {
+            System.out.println("There Is No Contact Available");
         } else {
-            System.out.println("Please Enter Valid Phone Number!");
-            editDetails();
+            System.out.println("Enter Phone Number Of A Person To Edit : ");
+            String editByPhoneNumber = sc.nextLine();
+            for (Address address : list) {
+                if ((address.getPhoneNumber().equals(editByPhoneNumber))) {
+
+                    System.out.print("1.Edit First Name  2.Edit Last Name  " +
+                            "3.Edit Address  4.Edit City Name  " +
+                            "5.Edit State Name  6.Edit Zip Code " +
+                            " 7.Edit Phone Number  8.Edit Email Id\n->");
+
+                    int option = sc.nextInt();
+                    sc.nextLine();
+
+                    switch (option) {
+                        case 1 -> {
+                            System.out.println("Enter New First Name : ");
+                            address.setFirstName(sc.nextLine());
+                        }
+                        case 2 -> {
+                            System.out.println("Enter New Last Name : ");
+                            address.setLastName(sc.nextLine());
+                        }
+                        case 3 -> {
+                            System.out.println("Enter New Address : ");
+                            address.setAddress(sc.nextLine());
+                        }
+                        case 4 -> {
+                            System.out.println("Enter New City Name : ");
+                            address.setCity(sc.nextLine());
+                        }
+                        case 5 -> {
+                            System.out.println("Enter New State Name : ");
+                            address.setState(sc.nextLine());
+                        }
+                        case 6 -> {
+                            System.out.println("Enter New Zip Code : ");
+                            address.setZip(sc.nextLine());
+                        }
+                        case 7 -> {
+                            System.out.println("Enter New Phone Number : ");
+                            address.setPhoneNumber(sc.nextLine());
+                        }
+                        case 8 -> {
+                            System.out.println("Enter New Email Id : ");
+                            address.setEmail(sc.nextLine());
+                        }
+                        default -> System.out.println("Please Choose Valid Option!");
+                    }
+
+                    System.out.println(address);
+                    System.out.println("Contact Edited Successfully.");
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                System.out.println("Please Enter Valid Phone Number!");
+            }
         }
     }
 
-    // In this deleteDetails function we are delete data by user input :-
+    // Here we are create a function to delete details from the arraylist by using user input :-
     public void deleteDetails() {
-        System.out.println("Enter Phone Number Of A Person To Delete : ");
-        String deleteByPhoneNumber = sc.nextLine();
-        if (address.getPhoneNumber().equals(deleteByPhoneNumber)) {
-            address = null;
-            System.out.println(address);
-            System.out.println("Details Deleted Successfully.");
+        boolean flag = false;
+        if (list.isEmpty()) {
+            System.out.println("There Is No Contact Available");
         } else {
-            System.out.println("Please Enter Valid Phone Number!");
-            deleteDetails();
+            System.out.println("Enter Phone Number Of A Person To Delete : ");
+            String deleteByPhoneNumber = sc.nextLine();
+            for (Address address : list) {
+                if (address.getPhoneNumber().equals(deleteByPhoneNumber)) {
+                    list.remove(address);
+                    System.out.println("Contact Deleted Successfully.");
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                System.out.println("Please Enter Valid Phone Number!");
+            }
+        }
+    }
+
+    // Here we are create a function to show details from the arraylist by using user input :-
+    public void showDetails() {
+        System.out.println("Number Of Contacts Stored : " + list.size());
+        if (list.isEmpty()) {
+            System.out.println("There Is No Contact Available");
+        } else {
+            list.forEach(System.out::println);
         }
     }
 }
--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+    public static final int ADD = 1;
+    public static final int EDIT = 2;
+    public static final int DELETE = 3;
+    public static final int VIEW = 4;
+    public static final int EXIT = 0;
+
     public static void main(String[] args) {
-        System.out.println("***** Welcome To Address Book System *****");
+        System.out.println("Welcome To Address Book System");
+        Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        addressBook.addDetails();
-        addressBook.editDetails();
-        addressBook.deleteDetails();
+        boolean exit = true;
+        while (exit) {
+
+            System.out.print("1.Add Contact ");
+            System.out.print(" 2.Edit Contact ");
+            System.out.print(" 3.Delete Contact ");
+            System.out.print(" 4.View Contact ");
+            System.out.print(" 0.Exit \n->");
+
+            int options = sc.nextInt();
+            switch (options) {
+                case ADD:
+                    addressBook.addDetails();
+                    break;
+                case EDIT:
+                    addressBook.editDetails();
+                    break;
+                case DELETE:
+                    addressBook.deleteDetails();
+                    break;
+                case VIEW:
+                    addressBook.showDetails();
+                    break;
+                case EXIT:
+                    exit = false;
+                    break;
+                default:
+                    System.out.println("Please Choose Valid Option!");
+                    break;
+            }
+        }
     }
 }
